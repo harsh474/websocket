d69e8683048ca6728b6ae235153c9f0de767ff8f
Harsh Rajput <harshrajput1101@gmail.com>
2024-07-18 01:39:36 +0530

    part-1 completed

--- client/app.js
+++ client/app.js
@@ -3,7 +3,7 @@ const socket = io("http://localhost:3001")
 const input = document.querySelector("input");  
 const form  = document.querySelector("form") ; 
 const ul  = document.querySelector("ul") ; 
-
+const p  = document.querySelector("p") ; 
 function sendMessgae(e){ 
     e.preventDefault();
     if(input.value){ 
@@ -14,10 +14,23 @@ function sendMessgae(e){
  } 
 form.addEventListener("submit",sendMessgae) ; 
 
-socket.on("messagedistributer",(data)=>{ 
+socket.on("message",(data)=>{ 
    console.log(data,"Message received from the server")  
       const li = document.createElement('li');
       li.append(data); 
       ul.appendChild(li);
 }); 
  
+input.addEventListener("keypress",(data)=>{ 
+    /// actitvity 
+    socket.emit("activity",socket.id.substring(0,4)) ;
+}) 
+
+socket.on("activity",(name)=>{ 
+    // p.textContent( `${data} is typing ....`);
+    p.textContent = `${name} is typing ...`;  
+    setTimeout(()=>{ 
+        p.textContent = "";
+    },3000);
+}); 
+
--- server/app.js
+++ server/app.js
@@ -6,23 +6,40 @@ const app = express();
 const httpServer = http.createServer(app);
 
 const io = new Server(httpServer, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
 io.on("connection", (socket) => {
-    console.log(`User ${socket.id} is connected`);
+  console.log(`User ${socket.id} is connected`);
+  // upon connection - only to current user
+  socket.emit("message", "welcome to Chat App");
 
-    socket.on("messagereciver", (data) => {
-        console.log(data);
-        io.emit("messagedistributer", `${socket.id.substring(0, 5)}: ${data}`);
-    });
+  // upon connection - to all other user
+  socket.broadcast.emit("message", `User ${socket.id.substring(0,5)}  connected`
+  );
+
+  // upon disconnection - to all user
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("message",`User ${socket.id.substring(0,5)} disconnected`
+    );
+  });
+
+  // capturing the activity event
+  socket.on("activity", (name) => {
+    socket.broadcast.emit("activity", name);
+  });
+
+  socket.on("messagereciver", (data) => {
+    console.log(data);
+    io.emit("message", `${socket.id.substring(0, 5)}: ${data}`);
+  });
 });
 
 const PORT = 3001;
 
 httpServer.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
